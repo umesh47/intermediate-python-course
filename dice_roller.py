f0ca006c92aa052227d2aa5f7aae90103de6149d
umesh47 <071bel2014@gmail.com>
2020-03-23 19:04:49 +0545

    I uncommented line 2

--- dice_roller.py
+++ dice_roller.py
@@ -1,19 +1,20 @@
-import random
+#import random
 
 def main():
-  dice_rolls = int(input('How many dice would you like to roll? '))
-  dice_size = int(input('HOw many sizes are dice? '))
-  dice_sum = 0
-  for i in range(0,dice_rolls):
-    roll = random.randint(1,dice_size)
-    dice_sum +=roll
-    if roll == 1:
-      print(f'You rolled a  {roll}! Critical Fail')
-    elif roll == dice_size:
-      print(f'You rolled a  {roll}! Critical Success!')
-    else:
-      print(f'You rolled a {roll}')
-  print(f'You have rolled a total of {dice_sum}')
+#  dice_rolls = int(input('How many dice would you like to roll? '))
+#  dice_size = int(input('HOw many sizes are dice? '))
+#  dice_sum = 0
+#  for i in range(0,dice_rolls):
+#    roll = random.randint(1,dice_size)
+#    dice_sum +=roll
+#    if roll == 1:
+#      print(f'You rolled a  {roll}! Critical Fail')
+#    elif roll == dice_size:
+#      print(f'You rolled a  {roll}! Critical Success!')
+#    else:
+#      print(f'You rolled a {roll}')
+#  print(f'You have rolled a total of {dice_sum}')
+  print('You rolled a die')
 
 if __name__== "__main__":
   main()
