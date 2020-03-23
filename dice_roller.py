d85c8a7278345e798733c8603bf4495cac5ad023
umesh47 <071bel2014@gmail.com>
2020-03-23 14:06:13 +0545

    conditional added

--- dice_roller.py
+++ dice_roller.py
@@ -1,16 +1,16 @@
 import random
 
 def main():
-  dice_rolls = int(input('How many dice woul you like to roll? '))
-  dice_size = int(input('HOw many sizes are dice? '))
+  dice_rolls = 2 # int(input('How many dice woul you like to roll? '))
+  #dice_size =  #int(input('HOw many sizes are dice? '))
   dice_sum = 0
   for i in range(0,dice_rolls):
-    roll = random.randint(1,dice_size)
+    roll = random.randint(1,6)
     dice_sum +=roll
     if roll == 1:
       print(f'You rolled a  {roll}! Critical Fail')
-    elif roll == dice_size:
-      print(f'You rolled a  {roll}! Critical Sucess!')
+    #elif roll == dice_size:
+    #  print(f'You rolled a  {roll}! Critical Sucess!')
     else:
       print(f'You rolled a {roll}')
   print(f'You have rolled a total of {dice_sum}')
